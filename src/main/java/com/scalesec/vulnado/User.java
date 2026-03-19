5d64b5f2687820b9e7fe351992d2425ac920572e
Julio Arruda <julio.arruda@outlook.com>
2026-03-19 11:40:57 -0300

    Print password during user retrieval
    Added a print statement to output the password for debugging purposes.

--- src/main/java/com/scalesec/vulnado/User.java
+++ src/main/java/com/scalesec/vulnado/User.java
@@ -51,6 +51,7 @@ public class User {
         String user_id = rs.getString("user_id");
         String username = rs.getString("username");
         String password = rs.getString("password");
+        System.out.println(password);
         user = new User(user_id, username, password);
       }
       cxn.close();
